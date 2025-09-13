09c16c7938589efb6ca003c1ae93438e550d3f8a
jwiggerthale <155433120+jwiggerthale@users.noreply.github.com>
2025-09-13 11:25:26 +0200

    Create ResNet18.py

---
+++ Classification/modules/ResNet18.py
@@ -0,0 +1,101 @@
+import torch.nn as nn
+import torch
+
+'''
+Basic block to build ResNet
+'''
+class BasicBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_channels != out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels)
+            )
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out += self.shortcut(x)
+        out = self.relu(out)
+        return out
+
+'''
+ResNet18 as basis for heteroscedastic model and H-UQNN
+initialize with: 
+  num_classes: int = 10 --> number of classes to be distinguished
+Note: Model has fully connected layer which is not used
+  --> forward pass only return features
+  --> features are further processed by H-UQNN's repsectively heteroscedastic model's heads
+'''
+class ResNet18(nn.Module):
+    def __init__(self, num_classes=10):
+        super(ResNet18, self).__init__()
+        self.in_channels = 64
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        
+        self.layer1 = self._make_layer(BasicBlock, 64, 2, stride=1)
+        self.layer2 = self._make_layer(BasicBlock, 128, 2, stride=2)
+        self.layer3 = self._make_layer(BasicBlock, 256, 2, stride=2)
+        self.layer4 = self._make_layer(BasicBlock, 512, 2, stride=2)
+        
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512, num_classes)
+
+    def _make_layer(self, block, out_channels, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_channels, out_channels, stride))
+            self.in_channels = out_channels
+        return nn.Sequential(*layers)
+
+    # forward pass (note: does only return features)
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.maxpool(out)
+        
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        
+        out = self.avgpool(out)
+        out = out.view(out.size(0), -1)
+        return out
+    
+
+# common classification model with dropout to model EU 
+class MC_Model(nn.Module):
+    def __init__(self, num_classes=10):
+        super(MC_Model, self).__init__()
+        self.feature_extractor = ResNet18()
+        num_features = 512
+        self.mu = nn.Sequential(nn.Linear(num_features, 80), 
+                                nn.ReLU(), 
+                                nn.Dropout(0.2),
+                                nn.Linear(80, 60),
+                                nn.ReLU(), 
+                                nn.Dropout(0.2),
+                                nn.Linear(60, num_classes)
+                                )
+    def forward(self, 
+                x: torch.tensor):
+        pred = self.feature_extractor(x)
+        pred = self.mu(pred)
+        return pred
