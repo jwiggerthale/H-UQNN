0eead1dcf48aa12f0c72a28b74e12b49f9eb6db7
jwiggerthale <155433120+jwiggerthale@users.noreply.github.com>
2025-09-14 08:25:52 +0200

    Add files via upload

---
+++ Classification/openood/utils/test.py
@@ -0,0 +1,4 @@
+cfgs = ['configs/datasets/cifar10/cifar10.yml', 
+        'configs/networks/dropout_net.yml', 
+        'configs/pipelines/train/baseline.yml',
+        'configs/preprocessors/base_preprocessor.yml']
